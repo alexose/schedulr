b692963399da1aedf03beee9aacadb3edcf69d20
Alexander Ose <alexander@parsely.com>
2022-09-24 09:45:10 -0700

    Add files

---
+++ src/Components/JobList.vue
@@ -0,0 +1,48 @@
+<script>
+    export default {
+        name: "JobList",
+        props: {
+            jobs: Array,
+        },
+        methods: {
+            async deleteJob(key) {
+                await fetch("/api/jobs/" + key, {
+                    method: "DELETE",
+                });
+            },
+        },
+    };
+</script>
+
+<template>
+    <table v-if="jobs.length" class="job-list">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Run</th>
+                <th>Last Run</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="job in jobs" :key="job.id">
+                <td>{{ job.id }}</td>
+                <td></td>
+                <td></td>
+                <td><button class="job-list-delete" @click="deleteJob(job.key)">X</button></td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<style>
+    .job-list {
+        width: 900px;
+        margin: 50px auto;
+        text-align: left;
+    }
+    .job-list ul {
+        list-style: none;
+    }
+    .job-list ul li {
+    }
+</style>
---
+++ src/Components/NewJob.vue
@@ -0,0 +1,112 @@
+<script>
+    import {VAceEditor} from "vue3-ace-editor";
+
+    export default {
+        name: "App",
+        components: {
+            VAceEditor,
+        },
+        methods: {
+            async addJob() {
+                const obj = {
+                    code: this.content,
+                };
+                if (this.every) {
+                    obj.every = this.every;
+                }
+                const response = await fetch("/api/jobs", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(obj),
+                });
+
+                const text = await response.text();
+                console.log(text);
+                this.jobForm = false;
+            },
+        },
+        data() {
+            return {
+                name: "",
+                content: "",
+                every: "5",
+                jobForm: false,
+            };
+        },
+    };
+</script>
+
+<template>
+    <div>
+        <button v-if="!jobForm" @click="this.jobForm = true">+ Add a new job</button>
+    </div>
+    <div v-if="jobForm" class="job-form">
+        <div class="job-form-option"><label>Name</label><input type="text" v-model="name" name="name" /></div>
+        <div class="job-form-option">
+            <label>Code</label>
+            <div class="editor">
+                <VAceEditor v-model:value="content" lang="html" theme="chrome" style="height: 300px" />
+            </div>
+        </div>
+        <div class="job-form-option">
+            <label>Run every</label>
+            <input type="text" class="job-form-input-small" maxlength="4" v-model="every" name="every" />
+            <label class="job-form-label-extra">minutes</label>
+        </div>
+        <div class="job-form-option">
+            <label></label>
+            <button type="submit" @click="addJob">Add Job</button>
+            <button @click="this.jobForm = false" class="secondary">Cancel</button>
+        </div>
+    </div>
+</template>
+
+<style>
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+    .job-form {
+        max-width: 800px;
+        margin: 0 auto;
+        text-align: left;
+    }
+    .job-form label {
+        margin: 5px 10px;
+        display: inline-block;
+        width: 100px;
+        text-align: right;
+    }
+    .job-form-option {
+        margin: 15px 0;
+        display: flex;
+    }
+    .job-form-option .editor {
+        border: 1px solid #ddd;
+        width: 630px;
+    }
+    .job-form-option input {
+        padding: 0 10px;
+        font-size: 16px;
+    }
+    .job-form-option button {
+        padding: 6px 10px;
+        margin-right: 20px;
+    }
+    .job-form-option button.secondary {
+        border: none;
+    }
+    .job-form-input-small {
+        width: 36px;
+        text-align: center;
+    }
+    .job-form label.job-form-label-extra {
+        width: auto;
+    }
+</style>
